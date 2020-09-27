b8f54c729ac121d7c5f3e220063896b114e4a1fc
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-27 22:19:31 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,11 @@
 require 'nokogiri'
+require 'pry'
 
 def create_project_hash
   
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
-end
+binding.pry 
+end 
+
